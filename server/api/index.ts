43884c39063b8b615779fa79271bb87bc4e4f718
Grace Wu <gracejwu@umich.edu>
2024-05-22 06:27:28 -0400

    vercel

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,17 +1,14 @@
-const express = require("express");
-const app = express();
+const express = require('express');
 const path = require('path');
-const f = require('node-fetch');
-require('dotenv').config()
+const nodeFetch = require('node-fetch');
+require('dotenv').config();
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
-  
+const app = express();
+
 const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
 const { IamAuthenticator } = require('ibm-watson/auth');
 
-const nlu= new NaturalLanguageUnderstandingV1({
+const nlu = new NaturalLanguageUnderstandingV1({
   version: '2022-04-07',
   authenticator: new IamAuthenticator({
     apikey: process.env.IBM_KEY,
@@ -19,146 +16,142 @@ const nlu= new NaturalLanguageUnderstandingV1({
   serviceUrl: 'https://api.us-east.natural-language-understanding.watson.cloud.ibm.com',
 });
 
-//get the quotes
-app.get('/api/quotes', async(req, res) =>{
-    const Headers = {
-        Accept: "application/json",
-        Authorization: `Bearer ${process.env.LOTR_KEY}`,
+// API routes
+app.get('/api/quotes', async (req, res) => {
+  const Headers = {
+    Accept: "application/json",
+    Authorization: `Bearer ${process.env.LOTR_KEY}`,
+  };
+  try {
+    const response = await nodeFetch('https://the-one-api.dev/v2/quote', { headers: Headers });
+    if (response.status !== 429) {
+      const quotes = await response.json();
+      const quoteData = quotes.docs[Math.floor(Math.random() * quotes.docs.length)];
+      const rawCharacters = await nodeFetch(`https://the-one-api.dev/v2/character?_id=${quoteData.character}`, { headers: Headers });
+      const characters = await rawCharacters.json();
+
+      const characterData = characters.docs[0];
+      const APIresponse = {
+        quote: quoteData.dialog,
+        char: characterData.name === "MINOR_CHARACTER" ? "Minor Character" : characterData.name,
+      };
+      res.json(APIresponse);
+    } else {
+      res.status(429).send('Rate limit exceeded');
     }
-    console.log('LOTR_KEY:', process.env.LOTR_KEY);
+  } catch (error) {
+    console.error('Error fetching quotes:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/api/analyze-quote-positive', async (req, res) => {
+  const headers = {
+    Accept: "application/json",
+    Authorization: `Bearer ${process.env.LOTR_KEY}`,
+  };
+  let attempts = 0;
+  const maxAttempts = 15;
+  while (attempts < maxAttempts) {
     try {
-      const response = await f('https://the-one-api.dev/v2/quote', { headers: Headers });
+      const response = await nodeFetch("https://the-one-api.dev/v2/quote", { headers });
       if (response.status !== 429) {
-        //fetching quote
-        const quotes = await response.json();
-        const quoteData = quotes.docs[Math.floor(Math.random() * quotes.docs.length)];
-        //fetching character
-        const rawCharacters = await f(`https://the-one-api.dev/v2/character?_id=${quoteData.character}`, { headers: Headers });
-        const characters = await rawCharacters.json();
+        const data = await response.json();
+        const allQuotes = data.docs;
+        const randomIndex = Math.floor(Math.random() * allQuotes.length);
+        const quote = allQuotes[randomIndex].dialog;
 
-        const characterData = characters.docs[0];
-        const APIresponse = {
-            quote: quoteData.dialog,
-            char: characterData.name === "MINOR_CHARACTER" ? "Minor Character" : characterData.name
+        if (quote.length > 15) {
+          const analyzeParams = {
+            text: quote,
+            features: { sentiment: {} },
+            language: "en",
           };
-          res.json(APIresponse);
+
+          const analysisResults = await nlu.analyze(analyzeParams);
+          const sentiment = analysisResults.result.sentiment.document.label;
+          if (sentiment === "positive") {
+            const rawCharacters = await nodeFetch(`https://the-one-api.dev/v2/character?_id=${allQuotes[randomIndex].character}`, {
+              headers,
+            });
+            const characters = await rawCharacters.json();
+
+            const char = characters.docs[0].name;
+            return res.json({ quote: quote, sentiment: sentiment, char: char });
+          }
+        }
+        attempts++;
       } else {
-        // Handle rate limit case, perhaps serve cached data or an error message
-        res.status(429).send('Rate limit exceeded');
+        const quotes = require('./quotesPositive.json');
+        const randomIndex = Math.floor(Math.random() * quotes.length);
+        return res.json({ quote: quotes[randomIndex].quote, char: quotes[randomIndex].character });
       }
-      
     } catch (error) {
-      console.error('Error fetching quotes:', error);
-      res.status(500).send('Internal Server Error');
+      console.log("error fetching quotes");
+      console.log(error);
+      break;
     }
-})
-
-//get the positive quote
-app.get('/api/analyze-quote-positive', async(req, res) => {
-    const headers = {
-        Accept: "application/json",
-        Authorization: `Bearer ${process.env.LOTR_KEY}`,
-      };
-    let attempts = 0
-    const maxAttempts = 15
-    while(attempts < maxAttempts){
-        try{
-            const response = await f("https://the-one-api.dev/v2/quote", {headers});
-            if(response.status !== 429){
-            const data = await response.json();
-            const allQuotes = data.docs;
-            const randomIndex = Math.floor(Math.random() * allQuotes.length);
-            const quote = allQuotes[randomIndex].dialog; 
-
-            //console.log(char);
-            if(quote.length > 15){
-                const analyzeParams = {
-                    text:  quote,
-                    features: {sentiment: {}},
-                    language: "en"
-                  };
-            
-                const analysisResults = await nlu.analyze(analyzeParams);
-                const sentiment = analysisResults.result.sentiment.document.label;
-                if(sentiment === "positive"){
-                    const rawCharacters = await f(`https://the-one-api.dev/v2/character?_id=${allQuotes[randomIndex].character}`,{
-                    headers
-                    });
-                    const characters = await rawCharacters.json(); 
-
-                    const char = characters.docs[0].name;
-                    return res.json({ quote: quote, sentiment: sentiment, char: char });
-                }
-            }
-            attempts++; 
-            }else{
-                const quotes = require('./quotesPositive.json');
-                const randomIndex = Math.floor(Math.random() * quotes.length);
-                return res.json({quote: quotes[randomIndex].quote, char: quotes[randomIndex].character})
-            }
-            
-        }catch(error){
-            console.log("error fetching quotes");
-            console.log(error);
-            break;
-        }
-    }
+  }
 });
 
-//get the negative quote
-app.get('/api/analyze-quote-negative', async(req, res) => {
-    const headers = {
-        Accept: "application/json",
-        Authorization: `Bearer ${process.env.LOTR_KEY}`,
-      };
-    let attempts = 0
-    const maxAttempts = 15
-    while(attempts < maxAttempts){
-        try{
-            const response = await f("https://the-one-api.dev/v2/quote", {headers});
-            if(response.status !== 429){
-            const data = await response.json();
-            const allQuotes = data.docs;
-            const randomIndex = Math.floor(Math.random() * allQuotes.length);
-            const quote = allQuotes[randomIndex].dialog; 
-            //console.log(char);
-            if(quote.length > 15){
-                const analyzeParams = {
-                    text:  quote,
-                    features: {sentiment: {}},
-                    language: "en"
-                  };
-            
-                const analysisResults = await nlu.analyze(analyzeParams);
-                const sentiment = analysisResults.result.sentiment.document.label;
-                console.log(quote);
-                console.log(sentiment);
-                if(sentiment === "negative"){
-                    const rawCharacters = await f(`https://the-one-api.dev/v2/character?_id=${allQuotes[randomIndex].character}`,{
-                    headers
-                    });
-                    const characters = await rawCharacters.json(); 
+app.get('/api/analyze-quote-negative', async (req, res) => {
+  const headers = {
+    Accept: "application/json",
+    Authorization: `Bearer ${process.env.LOTR_KEY}`,
+  };
+  let attempts = 0;
+  const maxAttempts = 15;
+  while (attempts < maxAttempts) {
+    try {
+      const response = await nodeFetch("https://the-one-api.dev/v2/quote", { headers });
+      if (response.status !== 429) {
+        const data = await response.json();
+        const allQuotes = data.docs;
+        const randomIndex = Math.floor(Math.random() * allQuotes.length);
+        const quote = allQuotes[randomIndex].dialog;
+        if (quote.length > 15) {
+          const analyzeParams = {
+            text: quote,
+            features: { sentiment: {} },
+            language: "en",
+          };
 
-                    const char = characters.docs[0].name;
-                    return res.json({quote: quote, sentiment: sentiment, char: char });
-                }
-            }
-            attempts++; 
-            }else{
-                const quotes = require('./quotesNegative.json');
-                const randomIndex = Math.floor(Math.random() * quotes.length);
-                return res.json({quote: quotes[randomIndex].quote, char: quotes[randomIndex].character})
-            }
-            
-        }catch(error){
-            console.log("error fetching quotes");
-            console.log(error);
-            break;
+          const analysisResults = await nlu.analyze(analyzeParams);
+          const sentiment = analysisResults.result.sentiment.document.label;
+          if (sentiment === "negative") {
+            const rawCharacters = await nodeFetch(`https://the-one-api.dev/v2/character?_id=${allQuotes[randomIndex].character}`, {
+              headers,
+            });
+            const characters = await rawCharacters.json();
+
+            const char = characters.docs[0].name;
+            return res.json({ quote: quote, sentiment: sentiment, char: char });
+          }
         }
+        attempts++;
+      } else {
+        const quotes = require('./quotesNegative.json');
+        const randomIndex = Math.floor(Math.random() * quotes.length);
+        return res.json({ quote: quotes[randomIndex].quote, char: quotes[randomIndex].character });
+      }
+    } catch (error) {
+      console.log("error fetching quotes");
+      console.log(error);
+      break;
     }
+  }
 });
 
-// Ensure your API routes are defined before this line
+// Middleware to serve static files after API routes
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
